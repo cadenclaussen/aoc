86c143eab60e5fb3f1644f34fd26eacb4d6a2513
Shane Claussen <sclaussen@yahoo.com>
2022-06-09 18:19:55 -0700

    Updates

--- 21d25.py
+++ 21d25.py
@@ -1,3 +1,14 @@
+2022 90.55
+2020 92.45
+2019 93.13
+2018 96.83
+2017 95.40
+2016 93.16
+2015 96.15
+2014 89.25
+2013 91.10
+
+
 with open("21d25.txt") as f:
     lines = f.read().strip().split("\n")
 
@@ -29,13 +40,19 @@ def step(floor):
 
 def eastHerd(floor):
     moves = False
+<<<<<<< HEAD
     changes = []
+=======
+    blanks = []
+    cucumbers = []
+>>>>>>> eb3dd30 (Updates)
     for y in range(len(floor)):
         for x in range(len(floor[0])):
             if floor[y][x] == ">":
                 if x == len(floor[0]) - 1:
                     if (floor[y][0] == "."):
                         # > is at the end of the row, '.' at beg, wrap the move
+<<<<<<< HEAD
                         changes.append([[y, x], [y, 0]])
 
                 elif floor[y][x + 1] == ".":
@@ -47,16 +64,43 @@ def eastHerd(floor):
         floor[change[1][0]][change[1][1]] = ">"
         moves = True
 
+=======
+                        # floor[y][x] = "."
+                        # floor[y][0] = ">"
+                        moves = True
+                        blanks.append([ y, x ])
+                        cucumbers.append([ y, 0 ])
+                elif floor[y][x + 1] == ".":
+                    # > is NOT at the end of the row
+                    moves = True
+                    # floor[y][x] = "."
+                    # floor[y][x + 1] = ">"
+                    blanks.append([ y, x ])
+                    cucumbers.append([ y, x + 1 ])
+                    # Skip the next x since it is our just moved ">"
+                    x += 1
+            x += 1
+    for blank in blanks:
+        floor[blank[0]][blank[1]] = '.'
+    for cucumber in cucumbers:
+        floor[cucumber[0]][cucumber[1]] = '>'
+>>>>>>> eb3dd30 (Updates)
     return floor, moves
 
 def southHerd(floor):
     moves = False
+<<<<<<< HEAD
     changes = []
+=======
+    blanks = []
+    cucumbers = []
+>>>>>>> eb3dd30 (Updates)
     for x in range(len(floor[0])):
         for y in range(len(floor)):
             if floor[y][x] == "v":
                 if y == len(floor) - 1:
                     if floor[0][x] == ".":
+<<<<<<< HEAD
                         changes.append([[y, x], [0, x]])
 
                 elif floor[y + 1][x] == ".":
@@ -67,6 +111,25 @@ def southHerd(floor):
         floor[change[1][0]][change[1][1]] = "v"
         moves = True
 
+=======
+                        moves = True
+                        # floor[y][x] = "."
+                        # floor[0][x] = "v"
+                        blanks.append([ y, x ])
+                        cucumbers.append([ 0, x ])
+                elif floor[y + 1][x] == ".":
+                    moves = True
+                    # floor[y][x] = "."
+                    # floor[y + 1][x] = "v"
+                    blanks.append([ y, x ])
+                    cucumbers.append([ y + 1, x ])
+                    y += 1
+            y += 1
+    for blank in blanks:
+        floor[blank[0]][blank[1]] = '.'
+    for cucumber in cucumbers:
+        floor[cucumber[0]][cucumber[1]] = 'v'
+>>>>>>> eb3dd30 (Updates)
     return floor, moves
 
 def printFloor(floor):
